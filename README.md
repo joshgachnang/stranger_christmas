f332eac3109893c255009c5ded54c2b2baff61c2
Josh Gachnang <josh@servercobra.com>
2016-12-23 13:27:06 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,46 @@
+Stranger Christmas
+==================
+
+This was a little project I did to learn how to use an Arduino. I love the Netflix
+series Stranger Things. When I saw the [Christmas lights scene](https://www.youtube.com/watch?v=_kn2e-bIlmE) (Spoilers!!),
+I (and probably every other nerd) went "ooo! I could make that...". So here it is!
+
+I've never used an Arduino before, so getting everything up and running was a lot of
+fun. I learned how easy it can be and now I want to make more projects.
+
+Merry Christmas!
+
+See It In Action!
+=================
+
+![](http://i.imgur.com/0zQ1AYA.gifv)
+
+[Here's an Imgur album of the prototype, the wiring, and some action shots](http://imgur.com/a/isnp1)
+
+Hardware and How To
+===================
+
+I used an [Arduino Uno](https://www.amazon.com/gp/product/B008GRTSV6?tag=serve01-20)
+to control the project. I used [this power supply](https://www.amazon.com/gp/product/B011QGJ8DG?tag=serve01-20).
+And these are the [WS2811 individually addressable lights](https://www.amazon.com/gp/product/B01AG923GI?tag=serve01-20).
+
+You can see [the wiring I did here](http://i.imgur.com/Aye0Iks.jpg). It is very 
+simple wiring and the breadboard probably isn't required any more. But it was 
+helpful for getting started and trying different things. Basically, you wire
+both grounds to the ground on the Arduino, connect the 5V directly to the Arduino 
+5V, and connect the data in wire on the lights to one of the data pins on the 
+Arduino (pin 3 in my case). That's it!
+
+You can open up the sketchbook in this repository with the Arduino IDE. Then just
+tap upload and it should start blinking away if everything is wired correctly! You
+may have to select your board and port under Tools -> Board and Tools -> Port,
+respectively. 
+
+To customize this, open the sketch in the Arduino IDE. You can change `BLINK_WORD`
+to blink any word you want. If you modify `colors`, you can have different colors
+picked randomly. You can change `NUM_LEDS` if you are stringing together more light
+strips. You can change `DATA_PIN` if your data in wire is not connected to data
+pin 3 on the Arduino. When you're done, hit upload, and your lights should start
+blinking!
+
+Note: These links are affiliate links.
---
+++ sketch_dec14a/sketch_dec14a.ino
@@ -0,0 +1,120 @@
+#include "FastLED.h"
+// Number of LEDs on your light string
+#define NUM_LEDS 50
+// Which data pin on the Arduino is hooked up to the Data In wire on your light string
+#define DATA_PIN 3
+#define GREEN CRGB::Red
+#define RED CRGB::Green
+#define BLUE CRGB::Blue
+#define BLACK CRGB::Black
+#define WHITE CRGB::White
+
+// This is the word that will be blinking, no spaces or special characters
+char BLINK_WORD [] = "MERRYCHRISTMAS";
+
+long randNumber;
+// Pick a random color from this array
+CRGB colors [4] = {RED, GREEN, BLUE, WHITE };
+CRGB color;
+CRGB leds[NUM_LEDS];
+
+// Map characters to LED numbers, starting with index 0. 
+// index 0 == 'A', 1 == 'B', etc
+// Change these to match how your lights are actually hung
+int characters [26] = {
+  2, // A 
+  3, // B
+  4, // C
+  5, // D
+  7, // E
+  8, // F
+  10, // G
+  11, // H
+  29, // I
+  28, // J
+  26, // K
+  25, // L
+  24, // M
+  23, // N
+  21, // O
+  20, // P
+  19, // Q
+  36, // R
+  37, // S
+  38, // T
+  40, // U
+  41, // V
+  43, // W
+  44, // X
+  46, // Y
+  47, // Z
+};
+
+// Map a lower case char to an LED
+int charToLED(char c) {
+  int i;
+  if (c >= 97 && c <= 122) {
+    // Lowercase
+    i = c - 97;
+  } else {
+    // Uppercase
+    i = c - 65;
+  }
+
+  return characters[i];
+}
+
+void initFlash() {
+  // Flash every light as white 
+  for ( int i = 0; i < NUM_LEDS; i = i + 1 ) {
+    leds[i] = WHITE;
+  }
+  
+  // Slowly go from off to full brightness
+  for ( int j = 0; j < 100; j = j + 1) {
+    FastLED.setBrightness(j);
+    delay(10);
+    FastLED.show();
+  }
+  
+  // Go from full brightness to off
+  for ( int j = 100; j >= 0; j = j - 1) {
+    FastLED.setBrightness(j);
+    delay(10);
+    FastLED.show();
+  }
+  
+  // Make the colored flashes full brightness
+  FastLED.setBrightness(100);
+  
+  // Reset the LEDs to black so we can blink the word
+  for ( int i = 0; i < NUM_LEDS; i = i + 1 ) {
+    leds[i] = BLACK;
+  }
+  delay(3000);
+}
+
+// Flash a random color for each character
+void blinkMessage() {
+  int x, c;
+  for ( int i = 0; i < sizeof(BLINK_WORD); i = i + 1 ) {
+    color = colors[random(4)];
+    c = BLINK_WORD[i];
+    x = charToLED(c);
+    leds[x] = color; FastLED.show(); delay(1000);
+    leds[x] = BLACK; FastLED.show(); delay(800);
+  }
+  delay(2000);
+}
+
+// Run when the Arduino boots up. Set up FastLED
+void setup() {
+  FastLED.addLeds<NEOPIXEL, DATA_PIN>(leds, NUM_LEDS);
+  randomSeed(analogRead(0));
+}
+
+// Run in a loop continuously
+void loop() {
+  initFlash();
+  blinkMessage();
+}
